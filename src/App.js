dd5a6cd6ef6c51f804f8d62a07d5ea7600c588d2
laura diaz <laura.diaz.mazzaferro@gmail.com>
2019-12-13 11:51:39 +0100

    Install @material-ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
+      {/*<CircularProgress color="secondary" />*/}
       <header className="App-header">
         holaa
       </header>
